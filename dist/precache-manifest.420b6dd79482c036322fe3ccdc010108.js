d115032d362caafc38fe339223f3471fe054e0d7
Margaret Mwaura <mwauramargaret1@gmail.com>
2020-08-13 20:18:54 +0300

    updating installed packages

---
+++ dist/precache-manifest.420b6dd79482c036322fe3ccdc010108.js
@@ -0,0 +1,9 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f916a549511a0912328d5626522d2c",
+    "url": "img/icon-up.svg"
+  },
+  {
+    "url": "main.d2e3c53cabc2803710f9.js"
+  }
+]);
