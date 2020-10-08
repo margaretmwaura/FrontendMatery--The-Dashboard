4c05b3f93dff263e1910943ee666e2da44568033
Margaret Mwaura <mwauramargaret1@gmail.com>
2020-10-08 23:56:58 +0300

    adding dist subtree

---
+++ precache-manifest.f77a0d8bdddd94b4d5c033c04a4619c3.js
@@ -0,0 +1,57 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a5c77f51678c145d64af371ff73b22",
+    "url": "/img/icon-down.svg"
+  },
+  {
+    "revision": "a738376499ac8a902fd2ce03c3030f98",
+    "url": "/img/icon-facebook.svg"
+  },
+  {
+    "revision": "991487fe03dace6f69a870d3085aadf1",
+    "url": "/img/icon-instagram.svg"
+  },
+  {
+    "revision": "810ccee84b0132ca7921a1b0b4ee6021",
+    "url": "/img/icon-twitter.svg"
+  },
+  {
+    "revision": "95f916a549511a0912328d5626522d2c",
+    "url": "/img/icon-up.svg"
+  },
+  {
+    "revision": "8499da129aedb70f3e20262951f7e236",
+    "url": "/img/icon-youtube.svg"
+  },
+  {
+    "revision": "a07f423b8bbf4a50c2c449b96795fcb6",
+    "url": "img/favicon-32x32.png"
+  },
+  {
+    "revision": "69a5c77f51678c145d64af371ff73b22",
+    "url": "img/icon-down.svg"
+  },
+  {
+    "revision": "a738376499ac8a902fd2ce03c3030f98",
+    "url": "img/icon-facebook.svg"
+  },
+  {
+    "revision": "991487fe03dace6f69a870d3085aadf1",
+    "url": "img/icon-instagram.svg"
+  },
+  {
+    "revision": "810ccee84b0132ca7921a1b0b4ee6021",
+    "url": "img/icon-twitter.svg"
+  },
+  {
+    "revision": "95f916a549511a0912328d5626522d2c",
+    "url": "img/icon-up.svg"
+  },
+  {
+    "revision": "8499da129aedb70f3e20262951f7e236",
+    "url": "img/icon-youtube.svg"
+  },
+  {
+    "url": "main.c73f69ea4aaa7d95453b.js"
+  }
+]);
